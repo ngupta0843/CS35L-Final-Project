f1bbf3ebc21b5706927ca0edcc4293ee00685def
ngupta0843 <ngupta0843@gmail.com>
2024-10-21 17:36:09 -0700

    test commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api",(req, res) => {
+    res.status(200).json({data: 'hello from /api'})
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
