e4204b48ff4024b54f9a44635976d4da97fe3c3c
aditya-am-murthy <aditya.am.murthy@gmail.com>
2024-11-11 18:25:26 -0800

    added mongo connection, and user create operation and workflow for API calls

---
+++ server/src/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('../configuration/dbconfig');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    saved_workouts: [String],
+    bio: String,
+    profileimage: String,
+    photos: [String],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
