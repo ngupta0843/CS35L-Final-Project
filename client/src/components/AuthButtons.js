faec9361b31141a9d5acf7921ac356085f21f9ca
ngupta0843 <ngupta0843@gmail.com>
2024-11-11 23:41:12 -0800

    added sign up page

--- client/src/components/AuthButtons.js
+++ client/src/components/AuthButtons.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { Button, Box } from '@mui/material';
-import './AuthButtons.css';
+import React from "react";
+import { Button, Box } from "@mui/material";
+import "./AuthButtons.css";
+import { useNavigate } from "react-router-dom";
 
 const AuthButtons = () => {
+  const navigate = useNavigate();
   return (
-    <Box className="auth-buttons" sx={{ position: 'absolute', top: 20, right: 20 }}>
+    <Box
+      className="auth-buttons"
+      sx={{ position: "absolute", top: 20, right: 20 }}
+    >
+      <Button
+        onClick={() => navigate("/")}
+        variant="outlined"
+        className="auth-button"
+        sx={{
+          color: "white",
+          borderColor: "white",
+          marginLeft: 2,
+          "&:hover": {
+            backgroundColor: "#fff",
+            color: "#121212",
+            boxShadow: "0 0 15px rgba(255, 255, 255, 1)",
+          },
+        }}
+      >
+        Home
+      </Button>
       <Button
         variant="outlined"
         className="auth-button"
         sx={{
-          color: 'white',
-          borderColor: 'white',
+          color: "white",
+          borderColor: "white",
           marginLeft: 2,
-          '&:hover': {
-            backgroundColor: '#fff',
-            color: '#121212',
-            boxShadow: '0 0 15px rgba(255, 255, 255, 1)',
+          "&:hover": {
+            backgroundColor: "#fff",
+            color: "#121212",
+            boxShadow: "0 0 15px rgba(255, 255, 255, 1)",
           },
         }}
       >
@@ -24,14 +46,15 @@ const AuthButtons = () => {
       <Button
         variant="outlined"
         className="auth-button"
+        onClick={() => navigate("/signup")}
         sx={{
-          color: 'white',
-          borderColor: 'white',
+          color: "white",
+          borderColor: "white",
           marginLeft: 2,
-          '&:hover': {
-            backgroundColor: '#fff',
-            color: '#121212',
-            boxShadow: '0 0 15px rgba(255, 255, 255, 1)',
+          "&:hover": {
+            backgroundColor: "#fff",
+            color: "#121212",
+            boxShadow: "0 0 15px rgba(255, 255, 255, 1)",
           },
         }}
       >
