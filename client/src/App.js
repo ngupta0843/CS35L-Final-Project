967ccf056a5e17f2f464c93d096ffedf7d3f4086
ngupta0843 <ngupta0843@gmail.com>
2024-11-13 13:38:36 -0800

    added user login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
+
+// function imports
 import LandingPage from "./pages/landing";
-import Login from "./components/auth/SignUp";
+import LogIn from "./components/auth/LogIn";
+import SignUp from "./components/auth/SignUp";
 import Header from "./components/Header";
-import Dashboard from  "./components/dashboard/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
 import AuthButtons from "./components/AuthButtons";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <CssBaseline />
         <Header />
         <AuthButtons />
         <Routes>
           <Route index element={<LandingPage />} />
-          <Route path="/signup" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LogIn />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
---
+++ client/src/components/auth/LogIn.jsx
@@ -0,0 +1,271 @@
+import React, { useState, memo } from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  Link,
+  Icon,
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import axios from "axios";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useNavigate } from "react-router-dom";
+
+const LogIn = memo(() => {
+  const navigate = useNavigate();
+  // useState hooks
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setUser({
+      ...user,
+      email: e.target.value,
+    });
+    console.log(user);
+  };
+
+  const handleSend = async () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(user.email)) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    try {
+      const response = await axios.post("http://localhost:8088/users/signin", {
+        email: user.email,
+        password: user.password,
+      });
+      console.log(response);
+      switch (response.status) {
+        case 200:
+          navigate("/dashboard", { state: { user: response.data.result } });
+          break;
+        default:
+          alert("Signup Failed");
+          break;
+      }
+    } catch (error) {
+      switch (error.response.status) {
+        case 404:
+          alert("Account doesn't exist");
+          break;
+        default:
+          alert("Signup Failed");
+          break;
+      }
+    }
+  };
+
+  return (
+    <Container
+      maxWidth="xl"
+      disableGutters
+      sx={{
+        display: "flex",
+        height: "100vh",
+        backgroundColor: "#121212",
+        width: "100%",
+      }}
+    >
+      {/* Left Section */}
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "#ffffff",
+          padding: 3,
+          backgroundColor: "#1c1c1c",
+        }}
+      >
+        <Box textAlign="center">
+          <Icon
+            component={FitnessCenterIcon}
+            sx={{ fontSize: "5rem", color: "#ffffff" }}
+          />
+          <Typography variant="h4" sx={{ fontWeight: "bold", mb: 2 }}>
+            Fitness is my Passion
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ fontSize: "1.2rem", width: "100%" }}
+          >
+            "I discovered the bootybuilder using this app and have not turned
+            back since." - Akarsh Legala, current student at UCLA
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* Right Section */}
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 3,
+          color: "#ffffff",
+          backgroundColor: "#121212",
+        }}
+      >
+        <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }}>
+          Sign into your account
+        </Typography>
+        <Typography variant="body2" sx={{ color: "#aaaaaa", mb: 3 }}>
+          Enter your email below to sign in.
+        </Typography>
+
+        {error && (
+          <Typography variant="body2" sx={{ color: "red", mb: 3 }}>
+            Error: Please enter a valid email address.
+          </Typography>
+        )}
+        {/* email textfield */}
+        <TextField
+          required
+          fullWidth
+          label="Email"
+          variant="filled"
+          type="email"
+          placeholder="name@example.com"
+          value={user.email}
+          onChange={handleEmailChange}
+          sx={{
+            mb: 2,
+            backgroundColor: "#333333",
+            borderRadius: 1,
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              "&:before": { borderBottomColor: "#666666" },
+              "&:hover:not(.Mui-disabled):before": {
+                borderBottomColor: "#aaaaaa",
+              },
+            },
+          }}
+          InputLabelProps={{
+            style: { color: "#aaaaaa" },
+          }}
+        />
+        {/* password textfield */}
+        <TextField
+          required
+          fullWidth
+          label="Password"
+          variant="filled"
+          onChange={(e) => {
+            e.preventDefault();
+            setUser({
+              ...user,
+              password: e.target.value,
+            });
+          }}
+          type={showPassword ? "text" : "password"}
+          placeholder="Enter your password"
+          sx={{
+            mb: 2,
+            backgroundColor: "#333333",
+            borderRadius: 1,
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              "&:before": { borderBottomColor: "#666666" },
+              "&:hover:not(.Mui-disabled):before": {
+                borderBottomColor: "#aaaaaa",
+              },
+            },
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  sx={{
+                    color: "#aaaaaa",
+                    "&:hover": {
+                      color: "#ffffff",
+                    },
+                  }}
+                  aria-label={
+                    showPassword ? "hide the password" : "display the password"
+                  }
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          InputLabelProps={{
+            style: { color: "#aaaaaa" },
+          }}
+        />
+
+        <Button
+          fullWidth
+          onClick={handleSend}
+          variant="contained"
+          sx={{
+            mb: 3,
+            backgroundColor: "#ffffff",
+            color: "#000000",
+            fontWeight: "bold",
+            "&:hover": { backgroundColor: "#e0e0e0" },
+          }}
+        >
+          Sign Up with Email
+        </Button>
+
+        <Divider sx={{ width: "100%", my: 2, color: "#aaaaaa" }}>
+          OR CONTINUE WITH
+        </Divider>
+
+        <Button
+          fullWidth
+          variant="outlined"
+          startIcon={<GitHubIcon />}
+          sx={{
+            color: "#ffffff",
+            borderColor: "#ffffff",
+            "&:hover": { backgroundColor: "#333333", borderColor: "#ffffff" },
+          }}
+        >
+          GitHub
+        </Button>
+
+        <Typography
+          variant="body2"
+          sx={{ color: "#aaaaaa", mt: 3, textAlign: "center" }}
+        >
+          By clicking continue, you agree to our{" "}
+          <Link href="#" sx={{ color: "#ffffff", textDecoration: "underline" }}>
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link href="#" sx={{ color: "#ffffff", textDecoration: "underline" }}>
+            Privacy Policy
+          </Link>
+          .
+        </Typography>
+      </Box>
+    </Container>
+  );
+});
+
+export default LogIn;
