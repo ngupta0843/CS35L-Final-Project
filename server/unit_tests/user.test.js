343308af354afac34546047b6c166f7fe98090b9
c-wei <caroline5wei@gmail.com>
2024-12-06 19:10:08 -0800

    user unit tests

--- server/unit_tests/user.test.js
+++ server/unit_tests/user.test.js
@@ -17,10 +17,6 @@ it('should get a list of users', function(done) {
                 done(err);  // If an error occurs, pass it to done
                 return;
             }
-
-            // Log the response to see what was returned
-            console.log("Response Body:", res.body);
-
             try {
                 // Assert that the response body is an array of users
                 expect(res.body).to.be.an('array');
@@ -38,3 +34,56 @@ it('should get a list of users', function(done) {
             }
         });
 });
+
+
+it('should get the current user', function(done) {
+    // Log the request to make sure the API is being hit correctly
+    console.log("Testing GET /users route");
+
+    chai.request(base_url)
+        .get('/currentUser?id=aditya@gmail.com')  // Assuming the route to fetch users is `/users`
+        .end(function (err, res) {
+            if (err) {
+                console.log("Error:", err);
+                done(err);  // If an error occurs, pass it to done
+                return;
+            }
+
+            try {
+
+                    expect(res.body).to.have.property('email');
+                
+                // Check the response status code
+                expect(res).to.have.status(200);
+                done();  // Signal the completion of the test
+            } catch (assertionError) {
+                console.log("Assertion Error:", assertionError);
+                done(assertionError);  // Pass assertion errors to done
+            }
+        });
+});
+
+
+// it('should send frend request', function(done) {
+//     // Log the request to make sure the API is being hit correctly
+//     console.log("Testing GET /users route");
+
+//     chai.request(base_url)
+//         .get('/sendFriendRequest?cur_user=aditya@gmail.com&req_user=caroline@gmail.com')  // Assuming the route to fetch users is `/users`
+//         .end(function (err, res) {
+//             if (err) {
+//                 console.log("Error:", err);
+//                 done(err);  // If an error occurs, pass it to done
+//                 return;
+//             }
+//             try {
+//                 // Check the response status code
+//                 expect(res).to.have.status(201);
+//                 done();  // Signal the completion of the test
+//             } catch (assertionError) {
+//                 console.log("Assertion Error:", assertionError);
+//                 done(assertionError);  // Pass assertion errors to done
+//             }
+//         });
+// });
+
